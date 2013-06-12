0388b7e7c43fba7d0c358b9a97d72d9861094768
Amit Dev <amitdev@gmail.com>
2013-06-12 19:14:10 +0530

    Update setup to pip enable

--- setup.py
+++ setup.py
@@ -3,7 +3,24 @@ from distutils.core import setup, Extension
 module1 = Extension('lru',
                     sources = ['lru.c'])
 
-setup (name = 'lru',
+setup (name = 'lru-dict',
        version = '1.0',
        description = 'An Dict like LRU container.',
-       ext_modules = [module1])
+       long_description = open('README.rst').read(),
+       author='Amit Dev',
+       url='https://github.com/amitdev/lru-dict',
+       license='MIT',
+       keywords='lru, dict',
+       ext_modules = [module1],
+       classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Operating System :: POSIX',
+        'Programming Language :: C',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+)
