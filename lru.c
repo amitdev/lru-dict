a90f68fe00c2a3de4367f676ab4014762b159dbb
Cong Ma <cong.ma@obspm.fr>
2019-08-19 11:54:58 +0200

    Add "setdefault" method.
    Implement the "setdefault" method which follows the semantics of
    Python's dict.setdefault.

--- lru.c
+++ lru.c
@@ -390,6 +390,29 @@ LRU_update(LRU *self, PyObject *args, PyObject *kwargs)
 }
 
 static PyObject *
+LRU_setdefault(LRU *self, PyObject *args)
+{
+    PyObject *key;
+    PyObject *default_obj = NULL;
+    PyObject *result;
+
+    if (!PyArg_ParseTuple(args, "O|O", &key, &default_obj))
+        return NULL;
+
+    result = lru_subscript(self, key);
+    PyErr_Clear();
+    if (result)
+        return result;
+
+    if (!default_obj)
+        default_obj = Py_None;
+
+    if (lru_ass_sub(self, key, default_obj) != 0)
+        return NULL;
+    return default_obj;
+}
+
+static PyObject *
 LRU_peek_first_item(LRU *self)
 {
     if (self->first) {
@@ -534,6 +557,8 @@ static PyMethodDef LRU_methods[] = {
                     PyDoc_STR("L.has_key(key) -> Check if key is there in L")},
     {"get",	(PyCFunction)LRU_get, METH_VARARGS,
                     PyDoc_STR("L.get(key, instead) -> If L has key return its value, otherwise instead")},
+    {"setdefault", (PyCFunction)LRU_setdefault, METH_VARARGS,
+                    PyDoc_STR("L.setdefault(key, default=None) -> If L has key return its value, otherwise insert key with a value of default and return default")},
     {"set_size", (PyCFunction)LRU_set_size, METH_VARARGS,
                     PyDoc_STR("L.set_size() -> set size of LRU")},
     {"get_size", (PyCFunction)LRU_get_size, METH_NOARGS,
